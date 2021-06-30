7b7590b45dae0e24e67c9603613779d29932a52e
noman2002 <nomn2002@gmail.com>
2021-07-01 00:45:27 +0530

    initial commits

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
+                 
+                 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
