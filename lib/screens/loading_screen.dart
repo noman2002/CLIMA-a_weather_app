b2c0c3c75ab469c6ac8bdfb2de9a12756e9244f4
noman2002 <nomn2002@gmail.com>
2021-06-30 23:25:09 +0530

    initial commits

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,12 +7,13 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
-
-
-  void getLocation()async{
-    Position position= await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+  void getLocation() async {
+    
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +21,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
